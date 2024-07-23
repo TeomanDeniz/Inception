97222aa41889298e9ac7ee2bbd21fe9a3d34442a
TeomanDeniz <te-odeniz@hotmail.com>
2024-07-23 05:35:32 +0300

    2024/07/23
    It's probably the last and working push of this project. Horay!

--- srcs/requirements/mariadb/tools/init.sh
+++ srcs/requirements/mariadb/tools/init.sh
@@ -16,10 +16,7 @@ while ! mariadb -u root -e "SELECT 1" >/dev/null 2>&1; do
   echo "Waiting for MariaDB to be ready..."
   sleep 1
 done
-mariadb << EOF
-CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE_NAME;
-CREATE USER IF NOT EXISTS '$MYSQL_DATABASE_USER'@'%' IDENTIFIED BY '$MYSQL_DATABASE_PASSWORD';
-GRANT ALL PRIVILEGES ON $MYSQL_DATABASE_NAME.* TO '$MYSQL_DATABASE_USER'@'%';
-FLUSH PRIVILEGES;
-SHUTDOWN;
-EOF
+mysql -u root -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;" || exit 1
+mysql -u root -e "CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';" || exit 1
+mysql -u root -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER}'@'%';" || exit 1
+mysql -u root -e "FLUSH PRIVILEGES;" || exit 1
--- srcs/requirements/wordpress/tools/entrypoint.sh
+++ srcs/requirements/wordpress/tools/entrypoint.sh
@@ -15,6 +15,23 @@ while ! wp db check --allow-root --path=/var/www/html/; do
 	echo "Waiting for Database to be ready..."
 	sleep 1
 done
+wp core download --path="/var/www/html/wordpress" --allow-root
+wp config create    --dbname="$MYSQL_DATABASE" \
+                    --dbuser="$MYSQL_USER" \
+                    --dbpass="$MYSQL_PASSWORD" \
+                    --dbhost="mariadb" \
+                    --allow-root
+wp core install     --url="$DOMAIN_NAME" \
+                    --title="Inception project" \
+                    --admin_user="superuser_wp" \
+                    --admin_password="$MYSQL_ROOT_PASSWORD" \
+                    --admin_email="$EMAIL" \
+                    --allow-root
+wp user create      "$MYSQL_USER" \
+                    "$EMAIL" \
+                    --role=author \
+                    --user_pass="$MYSQL_ROOT_PASSWORD" \
+                    --allow-root
 wp plugin install redis-cache --activate --allow-root --path=/var/www/html/
 wp redis enable --allow-root --path=/var/www/html/
 php-fpm7.4 -F
