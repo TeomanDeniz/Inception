dd93275b4c520ff1b2da5f86ba2759b95024fd32
TeomanDeniz <te-odeniz@hotmail.com>
2024-07-23 06:56:38 +0300

    2024/07/23
    Fuck you wp and it's bullsiht hash controls. Sorry but it's bullshit

--- srcs/requirements/wordpress/tools/entrypoint.sh
+++ srcs/requirements/wordpress/tools/entrypoint.sh
@@ -7,7 +7,7 @@
 #    By: hdeniz <Discord:@teomandeniz>              +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2024/07/22 12:48:13 by hdeniz            #+#    #+#              #
-#    Updated: 2024/07/22 12:51:01 by hdeniz           ###   ########.fr        #
+#    Updated: 2024/07/23 06:52:01 by hdeniz           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -15,10 +15,11 @@ while ! wp db check --allow-root --path=/var/www/html/; do
 	echo "Waiting for Database to be ready..."
 	sleep 1
 done
-wp core download --path="/var/www/html/wordpress" --allow-root
+test -d /var/www/html/wordpress || wp core download --path="/var/www/html/wordpress" --allow-root
+test -f /var/www/html/wordpress/wp-config.php && rm /var/www/html/wordpress/wp-config.php
 wp config create    --dbname="$MYSQL_DATABASE" \
                     --dbuser="$MYSQL_USER" \
-                    --dbpass="$MYSQL_PASSWORD" \
+                    --dbpass="$MYSQL_ROOT_PASSWORD" \
                     --dbhost="mariadb" \
                     --allow-root
 wp core install     --url="$DOMAIN_NAME" \
@@ -26,12 +27,12 @@ wp core install     --url="$DOMAIN_NAME" \
                     --admin_user="superuser_wp" \
                     --admin_password="$MYSQL_ROOT_PASSWORD" \
                     --admin_email="$EMAIL" \
-                    --allow-root
+                    --allow-root --path="/var/www/html/wordpress"
 wp user create      "$MYSQL_USER" \
-                    "$EMAIL" \
+                    "random@email.com" \
                     --role=author \
                     --user_pass="$MYSQL_ROOT_PASSWORD" \
                     --allow-root
 wp plugin install redis-cache --activate --allow-root --path=/var/www/html/
 wp redis enable --allow-root --path=/var/www/html/
-php-fpm7.4 -F
+php-fpm7.4 -F --nodaemonize
